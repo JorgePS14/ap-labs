eb19e118bab548a6bc8654a1cf67663045346656
Jorge Pietra Santa <a00226522@itesm.mx>
2020-02-18 20:15:53 -0600

    Solved C-Basics lab
    Signed-off-by: Jorge Pietra Santa <a00226522@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -6,12 +6,23 @@
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
+	if(argc < 2) {
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    		int fahr;
 
+    		for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+			printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	} else if(argc == 2) {
+		printf("Farenheit:: %3d, Celcius: %6.1f\n", atoi(argv[1]), (5.0/9.0)*(atoi(argv[1])-32));
+	} else {
+		int f;
+		int l = atoi(argv[1]);
+		int u = atoi(argv[2]);
+		int s = atoi(argv[3]);
+		for(f = l; f <= u; f = f+s)
+			printf("Fahrenheit: %3d, Celcius: %6.1f\n", f, (5.0/9.0)*(f-32));
+	}
     return 0;
 }
