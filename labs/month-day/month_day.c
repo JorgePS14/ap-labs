55ef63eef94b2030a7abab4cc34ffa9f01d38d3a
Jorge Pietra Santa <a00226522@itesm.mx>
2020-03-03 22:34:01 -0600

    Added month-day logic

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* month_day function's prototype*/
-void month_day(int year, int yearday, int *pmonth, int *pday);
+void month_day(int year, int yearday, char **pmonth, int *pday){
+	int months[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	char* monthNames[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-int main() {
-    return 0;
+	if(year % 4 == 0)
+		months[1] = 29;
+
+	if(year % 4 != 0 && yearday == 366)
+		return;
+
+	int month;
+	for(month = 0; months[month]; month++){
+		if(yearday > months[month])
+			yearday = yearday - months[month];
+		else
+			break;
+	}
+
+	*pmonth = monthNames[month];
+	*pday = yearday;
+}
+
+int main(int argc, char* argv[]) {
+	if(argc == 3) {
+		int year = atoi(argv[1]);
+		int yearday = atoi(argv[2]);
+		char* pmonth = "no";
+		int pday = 0;
+		if(yearday > 1 && yearday < 367)
+			month_day(year, yearday, &pmonth, &pday);
+		if(pmonth != "no" && pday != 0)
+			printf("Month: %s, day: %d, year: %d\n", pmonth, pday, year);
+		else
+			printf("Error: Yearday exceeded the number of days in that year\n");	
+	} else {
+		printf("Please check your input. Format: ./month_day <year> <yearday>\n");
+	}
+	return 0;
 }
