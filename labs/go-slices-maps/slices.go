bd9947e8f0f8e5fe47628300ee2f9235be90e27e
Jorge Pietra Santa <a00226522@itesm.mx>
2020-03-10 22:57:31 -0600

    Completed the tours for slices and maps in GoLang

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,23 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Picture(dx, dy int) [][]uint8 {
+	var arr [][]uint8
+
+	arr = make([][]uint8, dy)
+
+	for i := 0; i < len(arr); i++ {
+		arr[i] = make([]uint8, dx)
+
+		for j := 0; j < dx; j++ {
+			arr[i][j] = uint8(i) * (uint8(j)/2)
+		}
+	}
+
+	return arr
+}
+
 func main() {
+	pic.Show(Picture)
 }
