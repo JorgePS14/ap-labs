3f02e17fb19184ed7a706cd4951f58b1f99da09d
Jorge Pietra Santa <a00226522@itesm.mx>
2020-03-25 23:12:47 -0600

    Added strlib logic

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char*);
+char* mystradd(char*, char*);
+int mystrfind(char*, char*);
+
+int main(int argc, char** argv) {
+    if(argc < 4) {
+        printf("Please pass the 3 obligatory arguments.\n");
+        return -1;
+    }
+
+    int len = mystrlen(argv[1]);
+    char* added = mystradd(argv[1], argv[2]);
+    char* isSub = mystrfind(added, argv[3]) ? "Yes, it is." : "No, it is not.";
+    printf("Initial length:\t%d\n", len);
+    printf("New String:\t%s\n", added);
+    printf("Is it a substring?:\t%s\n", isSub);
     return 0;
 }
