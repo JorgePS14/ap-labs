c9c85041c5dc6c84585e39e6f563cbc99cd0360b
Jorge Pietra Santa <a00226522@itesm.mx>
2020-05-19 23:26:20 -0500

    Tried to solve web-crawler

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -1,64 +1,61 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 241.
-
-// Crawl2 crawls web links starting with the command-line arguments.
-//
-// This version uses a buffered channel as a counting semaphore
-// to limit the number of concurrent calls to links.Extract.
-//
-// Crawl3 adds support for depth limiting.
-//
 package main
 
 import (
 	"fmt"
 	"log"
 	"os"
+	"flag"
 
 	"gopl.io/ch5/links"
 )
 
-//!+sema
-// tokens is a counting semaphore used to
-// enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
 func crawl(url string) []string {
+
 	fmt.Println(url)
-	tokens <- struct{}{} // acquire a token
+	tokens <- struct{}{}
 	list, err := links.Extract(url)
-	<-tokens // release the token
+	<-tokens
 
 	if err != nil {
 		log.Print(err)
 	}
+
 	return list
 }
 
-//!-sema
-
-//!+
 func main() {
-	worklist := make(chan []string)
-	var n int // number of pending sends to worklist
 
-	// Start with the command-line arguments.
+	if len(os.Args) < 3 {
+		fmt.Println("Please use the correct format: go run crawl3.go -depth=<int:depth> <url-to-crawl>")
+		os.Exit(1)
+	}
+	worklist := make(chan []string)
+	var n int
+
 	n++
+
+	var d int
+	flag.IntVar(&d, "depth", 10, "Crawling depth")
+
 	go func() { worklist <- os.Args[1:] }()
 
-	// Crawl the web concurrently.
 	seen := make(map[string]bool)
+
 	for ; n > 0; n-- {
 		list := <-worklist
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+		
+		if d > 0 {
+			for _, link := range list {
+				if !seen[link] {
+					seen[link] = true
+					n++
+					d--
+					go func(link string) {
+						worklist <- crawl(link)
+					}(link)
+				}
 			}
 		}
 	}
